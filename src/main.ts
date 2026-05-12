5a9171f2b1e9e80212c36d0ff3f365d56352f2c9
Salamay <ayotundesalam16@gmail.com>
2026-05-12 22:45:53 +0100

    Admin

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
 
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'api.quanthex.io',
+    origin: 'quanthex-admin.web.app',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type,Authorization',
     credentials: true,
